04856bfdde12456aa67647c4b707c9d93e247e35
samuelgbenga <samuelgbenga972@gmail.com>
2024-11-25 17:01:00 +0100

    new commit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -47,7 +47,7 @@ export const Profile = () => {
               width={150}
               height={150}
               quality={100}
-              src="/ridding.png"
+              src="/meavatar.png"
               alt="Profile Picture"
               className="rounded-full size-12 md:w-full h-auto object-cover border-2"
             />
